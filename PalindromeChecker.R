e470950f129adb9b030cdfca175ed2c836e09e73
SuyogKhanal <countylover7@gmail.com>
2021-12-15 22:52:52 +0530

    third commit

---
+++ PalindromeChecker.R
@@ -0,0 +1,18 @@
+palindrome = function(n){
+  s = 0
+  temp = n
+  while(temp>0){
+    digit = temp%%10
+    s = s*10 + digit
+    temp = floor(temp/10)
+    
+    
+  }
+  if (s == n)
+  {
+    print("Palindrome")
+  }
+  else{
+    print("Not Palindrome")
+  }
+}
